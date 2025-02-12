12f291f58b8623faacf86ea454186b84b234a760
Berat Paban <67541132+beratpaban@users.noreply.github.com>
2025-02-12 20:48:41 +0300

    Lesson 11

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -32,7 +32,7 @@ exports.loginUser = async (req, res) => {
       });
     }
     req.session.userID = user._id;
-    res.status(200).redirect("/");
+    res.status(200).redirect("/users/dashboard");
   } catch (error) {
     res.status(400).json({
       status: "fail",
@@ -44,5 +44,13 @@ exports.loginUser = async (req, res) => {
 exports.logoutUser = (req, res) => {
   req.session.destroy(() => {
     res.redirect("/");
+  });
+};
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({ _id: req.session.userID });
+  res.status(200).render("dashboard", {
+    page_name: "dashboard",
+    user,
   });
 };
