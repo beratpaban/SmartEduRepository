abbf8c99ab5dd39a7ae6548d8471267c9a3adfdc
Berat Paban <67541132+beratpaban@users.noreply.github.com>
2025-02-12 19:05:04 +0300

    Lesson 08

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,11 +1,39 @@
 const User = require("../models/User");
-
+const bcrypt = require("bcrypt");
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
     res.status(201).json({
       status: "success",
       user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ status: "fail", message: "Kullanıcı bulunamadı" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Yanlış şifre",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      message: "Giriş yapıldı",
     });
   } catch (error) {
     res.status(400).json({
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,5 +4,6 @@ const authControllers = require("../controllers/authControllers");
 const router = express.Router();
 
 router.route("/signup").post(authControllers.createUser);
+router.route("/login").post(authControllers.loginUser);
 
 module.exports = router;
