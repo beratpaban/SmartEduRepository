7b50e7f49daca4cc75e3198e84548eec7250e35e
Berat Paban <67541132+beratpaban@users.noreply.github.com>
2025-02-13 12:31:51 +0300

    Lesson 14

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,8 @@
-const User = require("../models/User");
 const bcrypt = require("bcrypt");
+
+const User = require("../models/User");
+const Category = require("../models/Category");
+
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
@@ -46,8 +49,10 @@ exports.logoutUser = (req, res) => {
 
 exports.getDashboardPage = async (req, res) => {
   const user = await User.findOne({ _id: req.session.userID });
+  const categories = await Category.find();
   res.status(200).render("dashboard", {
     page_name: "dashboard",
     user,
+    categories,
   });
 };
